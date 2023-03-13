f8c9c1bbedbf09e0995f157c742753fe70a314cb
Eren <erennakarsu@gmail.com>
2023-03-13 17:02:32 +0100

    first commit

--- 121.py
+++ 121.py
@@ -3,10 +3,18 @@
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 # Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
-        l, r = 0, 1  # left= buy,  right= sell
-        maxP = 0
-        while r < len(prices):
-            # profitable
-            if prices[l] < prices[r]
+
+def maxProfit(prices: list[int]) -> int:
+    l, r = 0, 1  # left= buy,  right= sell
+    maxP = 0
+    while r < len(prices):
+        # profitable ?
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        else:
+            l = r
+        r += 1
+    return maxP
+
+print(maxProfit([7,1,5,3,6,4]))
