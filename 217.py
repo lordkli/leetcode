67cacdf46b1c41143615a5d124b25b4605996fd4
Eren <erennakarsu@gmail.com>
2023-03-13 17:18:33 +0100

    217

--- 217.py
+++ 217.py
@@ -8,3 +8,23 @@ def containsDuplicate(nums: list[int]) -> bool:
 
 print(containsDuplicate([1,2,3,1]))
 print(containsDuplicate([1,2,3,4]))
+
+
+def tekrarli_mi(liste):
+    for eleman in liste:
+        if liste.count(eleman) > 1:
+            return True
+    return False
+
+print(tekrarli_mi([1,2,3,1]))
+print(tekrarli_mi([1,2,3,4]))
+
+def tekrarli_mi2(liste):
+    sıralı_liste = sorted(liste)
+    for i in range(len(sıralı_liste)-1):
+        if sıralı_liste[i] == sıralı_liste[i+1]:
+            return True
+    return False
+
+print(tekrarli_mi2([1,2,3,1]))
+print(tekrarli_mi2([1,2,3,4]))
