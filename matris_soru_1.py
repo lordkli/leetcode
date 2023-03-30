758c75731269abc61411295207946e27882dbc54
Eren <erennakarsu@gmail.com>
2023-03-30 18:49:11 +0200

    matris

---
+++ matris_soru_1.py
@@ -0,0 +1,17 @@
+
+
+def solution(sayi:int)-> list[list[int]]:
+    deger = 10
+    temp = deger
+    azalis = 2
+    matris = [[0 for x in range(4)] for y in range(sayi)]
+    for i in range(sayi):
+        for j in range(4):
+            matris[i][j] = temp
+            temp -= azalis
+        deger += 10
+        temp = deger
+        azalis += 1
+    return matris
+
+print(solution(6))
