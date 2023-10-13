e4f1692c7a6ef12da2133e36f6939b5752e96b62
eren2 <erenakarsude@gmail.com>
2023-10-13 13:08:27 +0200

    543

--- sorular/Easy/tekrar_coz/543.py
+++ sorular/Easy/tekrar_coz/543.py
@@ -17,4 +17,16 @@ class TreeNode:
         self.right = right
 
 def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
+    res = [0]
     
+    def dfs(root):
+        if not root:
+            return -1
+        left = dfs(root.left)
+        right = dfs(root.right)
+        res[0] = max(res[0], 2 + left + right)
+    
+        return 1 + max (left, right)
+
+    dfs(root)
+    return res[0]
